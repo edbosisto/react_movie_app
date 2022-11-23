4811b50fa92fb27d16dc087734328b7a7669547d
edbosisto <e.bosisto@gmail.com>
2022-11-23 10:52:39 +0100

    retreived and added css and svg files from github

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
+
+// const API_KEY = 'bb6b6121';
+const API_URL = 'https://www.omdbapi.com/?apikey=bb6b6121';
+
 
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        // Get response from API
+        const response = await fetch(`${API_URL}&s=${title}`);
+        // Get data from the response
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Batman');
+    }, []);
+
     return (
-        <h1>App</h1>
+        <h1>MovieLand</h1>
     )
 }
 
